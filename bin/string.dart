53af4ded364ef2d523eb38a4264dfd3173e3f3c2
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-22 17:18:55 +0700

    oke

---
+++ bin/string.dart
@@ -0,0 +1,28 @@
+void main(){
+  String firstName = 'Nadif';
+  String lastName = "Zidan";
+
+  print(firstName);
+  print(lastName);
+
+  var fullName = '$firstName $lastName';
+  print(fullName);
+
+  var text = 'this is \'dart\' \$cool';
+  print(text);
+
+  var name1 = firstName + " " + lastName;
+  var name2 = 'Nadif' ' Zidan' ' Tazaka';
+
+  print(name1);
+  print(name2);
+
+  var longString = '''
+this is long string 
+multiline string
+learning dart
+ ''';
+
+  print(longString);
+
+}
