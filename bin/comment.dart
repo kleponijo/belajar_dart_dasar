ae03a9066eac2d0622942a75fc7a93ac275c9a23
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-22 16:41:59 +0700

    Create comment.dart

---
+++ bin/comment.dart
@@ -0,0 +1,14 @@
+
+/// this is main function
+/// will executed by dart
+void main(){
+  // this is name variable
+  var name = 'Nadif Zidan T';
+
+  /*
+   this is multi line comment
+  you can addmore here 
+  */ 
+  print(name);
+  
+}
