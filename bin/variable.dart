d277853c1e87647297d555540e76ac0b4b1ca5d1
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-22 16:37:09 +0700

    Update variable.dart

--- bin/variable.dart
+++ bin/variable.dart
@@ -1,11 +1,34 @@
 void main(){
 
-  print('Nadif Zidan T');
+  var name = "Nadif Zidan T";
 
-  print('Nadif Zidan T');
 
-  print('Nadif Zidan T');
+  print(name);
 
-  print('Nadif Zidan T');
+  print(name);
+
+  print(name);
+
+  print(name);
   
+  var firstName = 'Nadif';
+  final lastName = 'Zidan';
+
+  print(firstName);
+  print(lastName);
+
+  final array1 = [1,2,3];
+  const array2 = [1,2,3];
+
+  print(array1);
+  print(array2);
+
+  late var value = getValue();
+  print('Variable sudah dibuat');
+  print(value);
+}
+
+String getValue(){
+  print('getValue() dipanggil');
+  return 'Nadif Zidan Tazaka';
 }
