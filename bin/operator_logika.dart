27972863ea0f83e91746fb1a03bd6bb3786c3fb8
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-03-04 17:19:04 +0700

    oke

--- bin/operator_logika.dart
+++ bin/operator_logika.dart
@@ -1,7 +1,7 @@
 void main() {
 
-  var nilaiAkhir = 60;
-  var nilaiAbsen = 80;
+  var nilaiAkhir = 80;
+  var nilaiAbsen = 50;
 
   var nilaiAkhirBagus = nilaiAkhir >= 75;
   var nilaiAbsenBagus = nilaiAbsen >= 75;
@@ -9,7 +9,10 @@ void main() {
   print(nilaiAkhirBagus);
   print(nilaiAbsenBagus);
 
-  var lulus = nilaiAkhirBagus && nilaiAbsenBagus;
-
+  // var lulus = nilaiAkhirBagus && nilaiAbsenBagus;
+  var lulus = nilaiAkhirBagus || nilaiAbsenBagus;
   print(lulus);
+
+  print(!true);
+  print(!false);
 }
---
+++ bin/operator_type_test.dart
@@ -0,0 +1,8 @@
+void main() {
+  dynamic variable = 100;
+
+  var variableString = variable as String;
+
+  print(variable);
+  print(variableString);
+}
