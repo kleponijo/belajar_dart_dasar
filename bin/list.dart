e439552d26879babeef684b1d4709dc883f39a9b
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-03-04 21:44:35 +0700

    Create list.dart

---
+++ bin/list.dart
@@ -0,0 +1,18 @@
+void main() {
+  List<int> listInt= [];
+
+  var listString = <String>[];
+
+  print(listInt);
+  print(listString);
+
+  var names = <String>[];
+
+  names.add('Nadif');
+  names.add('Zidan');
+  names.add('Tazaka');
+
+  print(names);
+  print(names.length);
+
+}
