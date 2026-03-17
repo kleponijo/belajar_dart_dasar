094d5dc69f8343a1772d0eb2195aa2a4b00c63d5
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-03-17 14:22:38 +0700

    ok

---
+++ bin/switch_case.dart
@@ -0,0 +1,20 @@
+void main() {
+
+  var nilai = 'E';
+
+  switch(nilai) {
+    case 'A':
+    print('Anda lulus dengan baik');
+    break;
+    case 'B':
+    case 'C':
+    print('Anda lulus');
+    break;
+    case 'D':
+    print('Anda tidak lulus');
+    break;
+    default:
+    print('Mungkin anda salah jurusan');
+  }
+
+}
---
+++ bin/ternary_operator.dart
@@ -0,0 +1,15 @@
+void main() {
+
+var nilai = 65;
+String ucapan;
+
+if(nilai >= 75) {
+  ucapan = 'Selamat anda lulus';
+} else {
+  ucapan = 'Silahkan coba lagi';
+}
+
+print(ucapan);
+
+
+}
