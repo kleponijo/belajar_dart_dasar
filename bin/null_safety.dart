0fcb3b275c7b2690ce0f03999f95e361a069a5c3
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-03-18 04:13:01 +0700

    555555

---
+++ bin/null_safety.dart
@@ -0,0 +1,10 @@
+void main() {
+
+  int? age = null;
+  age = 1;
+
+if(age != null) {
+  double ageDouble = age.toDouble();
+  print(ageDouble);
+}
+}
--- bin/ternary_operator.dart
+++ bin/ternary_operator.dart
@@ -1,13 +1,13 @@
 void main() {
 
-var nilai = 65;
-String ucapan;
+var nilai = 85;
+var ucapan = nilai >= 75 ? 'Selamat anda lulus' : 'Silahkan coba lagi';
 
-if(nilai >= 75) {
-  ucapan = 'Selamat anda lulus';
-} else {
-  ucapan = 'Silahkan coba lagi';
-}
+// if(nilai >= 75) {
+//   ucapan = 'Selamat anda lulus';
+// } else {
+//   ucapan = 'Silahkan coba lagi';
+// }
 
 print(ucapan);
 
