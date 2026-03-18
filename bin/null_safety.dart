e18609160ba662834b0df33f42373b75fe8b39dd
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-03-18 11:00:56 +0700

    Update null_safety.dart

--- bin/null_safety.dart
+++ bin/null_safety.dart
@@ -17,12 +17,26 @@ void main() {
   }
 
   String? guest;
-  String guestName;
+   // guest = 'Nadif';
+   String guestName = guest ?? 'Guest';
+  //String guestName = guest != null ? guest : 'Guest'; // ternary operator
 
   // if (guest !=  null) {
   //   guestName = guest;
   // } else {
   //   guestName = 'Guest';
   // }
+
+  print(guestName);
+
+  int? nullableNumber;
+  // nullableNumber = 10;
+  // int nonullableNumber = nullableNumber!; // bisa error
+  // print(nonullableNumber);
   
+  int? dataInt;
+  double? dataDouble = dataInt?.toDouble();
+
+  print(dataDouble);
+
 }
