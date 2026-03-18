0a2325200e437ffd161b5f2ae1c52aa1622ba6dc
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-03-18 10:20:59 +0700

    Update null_safety.dart

--- bin/null_safety.dart
+++ bin/null_safety.dart
@@ -3,8 +3,26 @@ void main() {
   int? age = null;
   age = 1;
 
-if(age != null) {
+  if(age != null) {
   double ageDouble = age.toDouble();
   print(ageDouble);
-}
+  }
+
+  String name = 'Nadif';
+  String? nullableName = name;
+
+  int? nullablePrice = null;
+  if (nullablePrice != null) {
+    int price = nullablePrice;
+  }
+
+  String? guest;
+  String guestName;
+
+  // if (guest !=  null) {
+  //   guestName = guest;
+  // } else {
+  //   guestName = 'Guest';
+  // }
+  
 }
